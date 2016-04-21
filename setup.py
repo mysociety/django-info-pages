203cfab942197a3ed71565bb3d42ca494cf905e7
Duncan Parkes <duncan@mysociety.org>
2016-04-21 16:39:03 +0100

    Remove duplicate entry from requirements.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
         'Markdown',
         'django-markitup',
         'nose',
-        'django-slug-helpers',
         'django-autocomplete-light==2.3.3',
         'django-file-archive==0.0.2',
         'django-pagination==1.0.7',
