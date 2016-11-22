eb5fc5c8fd6a2ab0571aca1dee7acd9f0d74a4e6
Mark Longair <mhl@pobox.com>
2016-11-22 15:35:10 +0000

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "django-info-pages",
-    version = "0.0.3",
+    version = "0.0.4",
     packages = find_packages(),
     author = "Edmund von der Burg",
     author_email = "evdb@ecclestoad.co.uk",
